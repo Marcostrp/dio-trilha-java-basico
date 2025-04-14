d4aad9aee2bdafecf8e6866eea6cf463e67cc1ca
Marcostrp <marcostrp@gmail.com>
2025-04-14 11:15:11 -0300

    atualização

---
+++ tipos-variaveis/src/TiposEVariáveis.java
@@ -0,0 +1,13 @@
+public class TiposEVariáveis {
+    public static void main(String[] args) throws Exception {
+        byte b = 100; // numeros inteiros de -128 a 127
+        short s = 30000; // números inteiros de -32000 a 32000
+        int i = 345345; // números inteiros extensos
+        long l = 1234567877878L; // números muito extensos, aplica-se o L no final
+        float f = 3.14f; // números com casas decimais, aplica-se o f ao final
+        double d = 4.123445; // números com casas decimais extensos
+        String nome = "Sequência de caracteres"; // textos
+        char c = 'A'; // caractere único, apenas um caractere entre aspas simples
+        boolean b1 = true; // true ou false, verdadeiro ou faso, sim ou não
+    }
+}
